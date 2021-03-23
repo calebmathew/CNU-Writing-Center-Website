8ca22c6dcdee1dcb26a43ebc428e8071a8e7473c
Caleb Mathew <caleb.mathew.17@cnu.edu>
2021-03-23 16:15:43 -0400

    Replace insert.php

--- insert.php
+++ insert.php
@@ -1,3 +1,27 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title> CNU Writing Center</title>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, intial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="capstone.css" rel="stylesheet">
+</head>
+<body>
+    <nav>
+        <img src="images/pic5.png" width="125" height="70" style="float: left" alt="CNU Logo" />
+        <ul>
+            <li><a href="index.html">Home</a></li>
+		    <li><a href="about.html">About Us</a></li>
+            <li><a href="resources.html">Resources</a></li>
+            <li><a href="training.html">Consultant Training</a></li>
+            <li><a href="tidewater.html">Tidewater Community Writing Center</a></li>
+        </ul>
+    </nav>
+    <header>
+        <h1>Welcome to the Alice F. Randall Writing Center Webpage!</h1>
+    </header>
+    <div class="container">
 <?php
 
 $conn = mysqli_connect('cjm-capstonedb.c9glmfvid2ev.us-east-1.rds.amazonaws.com', 'CJM00948910', '1Eyeball', 'Capstone', '3306');
@@ -28,3 +52,9 @@ if (isset($_POST['submit'])) {
 }
 
 ?>
+    </div>
+    <footer>
+        Copyright &copy; CNU Writing Center 2021
+    </footer>
+</body>
+</html>
