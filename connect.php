d9b7465851c23617095e9506944e72a5d0e4ddf4
Caleb Mathew <caleb.mathew.17@cnu.edu>
2021-03-23 15:58:47 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html>
+<html lang='en'>
+<body>
 <?php
 $mysqli = mysqli_connect('cjm-capstonedb.c9glmfvid2ev.us-east-1.rds.amazonaws.com', 'CJM00948910', '1Eyeball', 'Capstone', '3306');
 
@@ -9,3 +12,5 @@ else {
     mysqli_close($mysqli);
 }
 ?>
+</body>
+</html>
