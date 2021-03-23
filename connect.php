fce86099d7543524f5fab556176c4be95c14cda8
Caleb Mathew <caleb.mathew.17@cnu.edu>
2021-03-23 16:22:00 -0400

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,14 +3,6 @@
 <body>
 <?php
 $mysqli = mysqli_connect('cjm-capstonedb.c9glmfvid2ev.us-east-1.rds.amazonaws.com', 'CJM00948910', '1Eyeball', 'Capstone', '3306');
-
-if ($mysqli->connect_error) {
-    die('connection failed: ' .$mysqli->connect_error);
-}
-else {
-    echo 'connected successfully';
-    mysqli_close($mysqli);
-}
 ?>
 </body>
 </html>
