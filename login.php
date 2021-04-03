e954b579607ed44500ac5e3ee10749d27261f2a5
Caleb Mathew <caleb.mathew.17@cnu.edu>
2021-04-03 18:20:04 -0400

    Upload New File

---
+++ login.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+    <title> CNU Writing Center</title>
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, intial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <link href="capstone.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+</head>
+<body>
+    <nav>
+        <img src="images/pic5.png" width="145" height="60" style="float: left" alt="CNU Logo" />
+        <ul>
+            <li><a href="index.php">Home</a></li>
+		    <li><a href="about.html">About Us</a></li>
+            <li><a href="resources.html">Resources</a></li>
+            <li><a href="training.html">Consultant Training</a></li>
+            <li><a href="tidewater.html">Events</a></li>
+            <li><a href="login.php">For Staff</a></li>
+        </ul>
+    </nav>
+    <main>
+        <div class="login">
+            <h3>Login</h3>
+            <form action="autheticate.php" method="post">
+                <label for="username">
+                    <i class="fas fa-user"></i>
+                </label>
+                <input type="text" name="username" placeholder="Username" id="username" required>
+                <label for="password">
+                    <i class="fas fa-lock"></i>
+                </label>
+                <input type="password" name="password" placeholder="Password" id="password" required>
+                <input type="submit" value="Login">
+            </form>
+        </div>
+    </main>
+    <footer>
+        Copyright &copy; CNU Writing Center 2021
+    </footer>
+</body>
+</html>
