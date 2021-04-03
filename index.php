b3c371d4f3abdf9b900d30da82c16a7b467872b2
Caleb Mathew <caleb.mathew.17@cnu.edu>
2021-04-03 13:59:47 -0400

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 </head>
 <body>
     <nav>
-        <img src="images/pic5.png" style="float: left" alt="CNU Logo" />
+        <img src="pic5.png" style="float: left" alt="CNU Logo" />
             
         <a href="https://www.facebook.com/pg/cnuwritingcenter/about/" class="fa fa-facebook" style="float: right"></a>
         <a href="https://twitter.com/cnuwritingcntr?lang=en" class="fa fa-twitter" style="float: right"></a>
@@ -34,9 +34,9 @@
         <div class="container">
             <br>
             <div class="w3-content w3-display-container">
-            <img class="mySlides" src="images/pic1.jpg" style="width:100%">
-            <img class="mySlides" src="images/pic2.jpg" style="width:100%">
-            <img class="mySlides" src="images/pic3.jpg" style="width:100%">
+            <img class="mySlides" src="pic1.jpg" style="width:100%">
+            <img class="mySlides" src="pic2.jpg" style="width:100%">
+            <img class="mySlides" src="pic3.jpg" style="width:100%">
 
             <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
             <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
