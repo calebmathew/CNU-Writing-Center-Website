488f8a7d69a006268edcdfe9c7914e85ee8bb0f8
Caleb Mathew <caleb.mathew.17@cnu.edu>
2021-04-12 13:26:01 -0400

    Update staff.php

--- staff.php
+++ staff.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['loggedin'])) {
 </head>
 <body>
     <nav>
-        <img src="pic5.png" width="145" height="60" style="float: left" alt="CNU Logo" />
+        <img src="images/pic5.png" width="145" height="60" style="float: left" alt="CNU Logo" />
         <a href="https://www.facebook.com/pg/cnuwritingcenter/about/" class="fa fa-facebook" style="float: right"></a>
         <a href="https://twitter.com/cnuwritingcntr?lang=en" class="fa fa-twitter" style="float: right"></a>
         <a href="https://www.instagram.com/cnuwritingcenter/?hl=en" class="fa fa-instagram" style="float: right"></a>
@@ -35,18 +35,29 @@ if (!isset($_SESSION['loggedin'])) {
     <main>
         <p>Welcome back, <?=$_SESSION['name']?>!</p>
     </main>
+        <div class="container">
                 <?php 
                         $sql = 'SELECT content, email FROM questions';
                         $result = mysqli_query($conn, $sql);
                         if (mysqli_num_rows($result) > 0) {
-                            while($row = mysqli_fetch_assoc($result)) {
-                                echo "Content: " . $row['content'] . "Email:" . $row['email'] . "<br>";
+                            echo "<table";
+                                echo "<tr>";
+                                    echo "<th>Content</th>";
+                                    echo "<th>Email</th>";
+                                echo "<tr>";
+                            while($row = mysqli_fetch_array($result)) {
+                                echo "<tr>";
+                                    echo "<td>" . $row['content'] . "</td>";
+                                    echo "<td>" . $row['email'] . "</td>";
+                                echo "</tr>";
                             }
+                            echo "</table>";
                         } else{
                             echo "0 results";
                         }
                         mysqli_close($conn);
                 ?>
+        </div>
     <footer>
         Copyright &copy; CNU Writing Center 2021
     </footer>
