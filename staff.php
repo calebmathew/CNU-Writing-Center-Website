77e98dcfb51481b59f209f588c297ac028d3d849
Caleb Mathew <caleb.mathew.17@cnu.edu>
2021-04-17 16:06:14 -0400

    Update staff.php

--- staff.php
+++ staff.php
@@ -68,7 +68,6 @@ if (!isset($_SESSION['loggedin'])) {
                         } else{
                             echo "0 results";
                         }
-                        mysqli_close($conn);
                 ?>
         </div>
         <br>
